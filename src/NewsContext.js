a7ba77f979d55a172e9b67e34f10825ebe4dd449
Diliru Munasingha <diliru1996@gmail.com>
2022-01-26 07:27:39 +0530

    initial commit

---
+++ src/NewsContext.js
@@ -0,0 +1,21 @@
+import React, { createContext, useEffect, useState } from 'react';
+import axios from 'axios';
+
+export const NewsContext = createContext();
+
+export const NewsContextProvider = (props) => {
+  const [data, setData] = useState();
+
+  useEffect(() => {
+    axios
+      .get(`http://localhost:5001/v1/news`)
+      .then((response) => setData(response.data))
+      .catch((error) => console.log(error));
+  }, []);
+
+  return (
+    <NewsContext.Provider value={{ data }}>
+      {props.children}
+    </NewsContext.Provider>
+  );
+};
